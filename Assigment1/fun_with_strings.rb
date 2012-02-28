8d121c6198e20ed8afe70d3978f7de4577f929a7
Juan Felipe Arango <jfelipe38@gmail.com>
2012-02-28 18:15:29 +0100

    Using inject for count_words method

--- Assigment1/fun_with_strings.rb
+++ Assigment1/fun_with_strings.rb
@@ -2,15 +2,10 @@
 
 def palindrome?(string)
   str1 = string.gsub(/(\W|\b)+/, "").downcase
-  str2 = str1.reverse
-  str1 == str2
+  str1 == str1.reverse
 end
 
 def count_words(string)
   words = string.downcase.split(/\b\W*/)
-  hash = Hash.new(0)
-  words.each do |word|
-    hash[word] += 1
-  end
-  hash
+  words.inject(Hash.new(0)) { |res, word| res[word] += 1; res }
 end
