9c808b5224dd5d0d271201cff0fb87380b9dd157
Juan Felipe Arango <jfelipe38@gmail.com>
2012-03-05 17:48:17 +0100

    Adding basic oop

---
+++ Assigment1/basic_oop.rb
@@ -0,0 +1,33 @@
+#Assigment1 basic_oop - Juan Felipe Arango - 
+class Dessert
+  attr_accessor :name, :calories
+  
+  def initialize(name, calories)
+    self.name = name
+    self.calories = calories  
+  end
+
+  def healthy?
+    calories < 200
+  end
+
+  def delicious?
+    true
+  end
+
+end
+
+class JellyBean < Dessert
+  attr_accessor :flavor
+  
+  def initialize(name, calories, flavor)
+    self.flavor = flavor
+    super name, calories
+  end
+
+  def delicious?
+    return false if flavor == "black licoric"
+    true
+  end
+
+end
